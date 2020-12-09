9913a139a01f22fc396e906f946a6a5783b87c2b
zhk <1908180749@qq.com>
2020-12-09 08:21:47 +0800

    home分支

---
+++ demo/src/components/Home.vue
@@ -0,0 +1,24 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-09 08:20:19
+ * @LastEditTime: 2020-12-09 08:20:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \1209\demo\src\components\Home.vue
+-->
+
+<template>
+  <div>
+      <h1>我是home页</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
