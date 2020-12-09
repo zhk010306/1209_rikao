94c58aec3d08cdfc7c31d607ab43bd81a1c6cfc0
zhk <1908180749@qq.com>
2020-12-09 08:16:04 +0800

    修改

---
+++ demo/src/App.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-09 08:10:01
+ * @LastEditTime: 2020-12-09 08:14:15
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \1209\demo\src\App.vue
+-->
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
---
+++ demo/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-09 08:10:01
+ * @LastEditTime: 2020-12-09 08:15:24
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \1209\demo\src\components\HelloWorld.vue
+-->
+<template>
+  <div class="hello">
+    {{this.msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
